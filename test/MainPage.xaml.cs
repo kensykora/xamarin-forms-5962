1a5a26b5beba8a76961bacbf71e679142a5e44b4
Ken Sykora <ken.sykora@toro.com>
2019-04-19 16:19:56 -0500

    Add Example

---
+++ test/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            web.Navigated += Web_Navigated;
+
+            web.Source = new UrlWebViewSource 
+            {
+                Url = "https://ionxskmskstglog.blob.core.windows.net/test/index.html",
+            };
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            web.Reload();
+        }
+
+        void Web_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            status.Text = e.Result.ToString();
+        }
+
+    }
+}
